ccf19e50049325359deafdcd2baa1882799a6d8b
Daffa Audya <daffaaudyapramana03@gmail.com>
2023-11-22 00:34:15 +0700

    update test

--- wgt_test.go
+++ wgt_test.go
@@ -9,23 +9,37 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func TestGeneratePasswordHash(t *testing.T) {
-	password := "kepoih"
-	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+// PASETO
+func TestGeneratePrivateKeyPaseto(t *testing.T) {
+	privateKey, publicKey := watoken.GenerateKey()
+	fmt.Println(privateKey)
+	fmt.Println(publicKey)
+	hasil, err := watoken.Encode("wegotour", privateKey)
+	fmt.Println(hasil, err)
+}
+func TestValidateToken(t *testing.T) {
+	tokenstring := "eyJleHAiOiIyMDIzLTEwLTI2VDA1OjAyOjQ1WiIsImlhdCI6IjIwMjMtMTAtMjZUMDM6MDI6NDVaIiwiaWQiOiJkYWZmYSIsIm5iZiI6IjIwMjMtMTAtMjZUMDM6MDI6NDVaIn3cLq58WoqF4cfwdtKZiUas4-p4PVbwDaF4sa0QConAH_hZWT726D8" // Gantilah dengan token PASETO yang sesuai
+	publicKey := "34984c89d5553bd07ced0b9ed6306cc010418a1758fae39e92bfce521ee7b44e"
+	payload, _err := watoken.Decode(publicKey, tokenstring)
+	if _err != nil {
+		fmt.Println("expired token", _err)
+	} else {
+		fmt.Println("ID: ", payload.Id)
+		fmt.Println("Di mulai: ", payload.Nbf)
+		fmt.Println("Di buat: ", payload.Iat)
+		fmt.Println("Expired: ", payload.Exp)
+	}
+}
 
+// Hash Pass
+func TestGeneratePasswordHash(t *testing.T) {
+	password := "bisabis15"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
 	fmt.Println("Password:", password)
 	fmt.Println("Hash:    ", hash)
 
 	match := CheckPasswordHash(password, hash)
 	fmt.Println("Match:   ", match)
-}
-
-func TestGeneratePrivateKeyPaseto(t *testing.T) {
-	privateKey, publicKey := watoken.GenerateKey()
-	fmt.Println("Private Key: ", privateKey)
-	fmt.Println("Public Key: ", publicKey)
-	hasil, err := watoken.Encode("wegotour", privateKey)
-	fmt.Println("hasil: ", hasil, err)
 }
 
 func TestHashFunction(t *testing.T) {
