cfc521660e6235307a7f5fa6e7a5c019858831dc
Daffa Audya <daffaaudyapramana03@gmail.com>
2023-11-25 18:15:56 +0700

    update type

--- type.go
+++ type.go
@@ -7,6 +7,13 @@ type User struct {
 	Role     string `json:"role,omitempty" bson:"role,omitempty"`
 }
 
+type Admin struct {
+	Email    string `bson:"email,omitempty" json:"email,omitempty"`
+	Username string `bson:"username,omitempty" json:"username,omitempty"`
+	Password string `bson:"password,omitempty" json:"password,omitempty"`
+	Role     string `json:"role,omitempty" bson:"role,omitempty"`
+}
+
 type Credential struct {
 	Status  bool   `json:"status" bson:"status"`
 	Token   string `json:"token,omitempty" bson:"token,omitempty"`
